50349b8568e1dda9ddb21139d30b54fdbf17fda0
Clarence Chan <clarencechaan@icloud.com>
2022-07-03 17:59:28 -0400

    start search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,20 @@
 import "../styles/SearchBar.css";
+import { MagnifyingGlass, ArrowLeft } from "phosphor-react";
 
 function SearchBar() {
-  return <div className="SearchBar"></div>;
+  return (
+    <div className="SearchBar">
+      <input type="text" placeholder="Search VisagePad" />
+      <MagnifyingGlass className="magnifying-glass" />
+      <div className="window">
+        <div className="top-bar">
+          <button className="back-btn">
+            <ArrowLeft className="arrow-left" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SearchBar;
