065d25d044779c917c4cc1a4383bb1c4b9532101
Clarence Chan <clarencechaan@icloud.com>
2022-07-04 19:48:29 -0400

    persist search window when focused

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,12 +3,17 @@ import { MagnifyingGlass, ArrowLeft } from "phosphor-react";
 
 function SearchBar() {
   return (
-    <div className="SearchBar">
+    <div className="SearchBar" tabIndex={"-1"}>
       <input type="text" placeholder="Search VisagePad" />
       <MagnifyingGlass className="magnifying-glass" />
       <div className="window">
         <div className="top-bar">
-          <button className="back-btn">
+          <button
+            className="back-btn"
+            onClick={(e) => {
+              e.target.blur();
+            }}
+          >
             <ArrowLeft className="arrow-left" />
           </button>
         </div>
