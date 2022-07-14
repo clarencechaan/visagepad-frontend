265738ffc276c6bf278621db8b6d10f4ffd0931c
Clarence Chan <clarencechaan@icloud.com>
2022-07-14 17:05:49 -0400

    handle image uploading

--- src/components/NewPostBtn.js
+++ src/components/NewPostBtn.js
@@ -2,23 +2,52 @@ import "../styles/NewPostBtn.css";
 import profilePic from "../images/profile-pic.jpeg";
 import { Image } from "phosphor-react";
 import { useRef, useState } from "react";
+import Throbber from "./Throbber";
 
 function NewPostBtn() {
-  const photoPreviewRef = useRef(null);
-  const photoPickerRef = useRef(null);
-  const photoPickerInput = useRef(null);
   const textInputRef = useRef(null);
   const [newPostFormShown, setNewPostFormShown] = useState(false);
+  const [postContent, setPostContent] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  function handlePhotoPicked() {
-    photoPreviewRef.current.hidden = false;
-    photoPickerRef.current.hidden = true;
+  async function uploadImage(file) {
+    const CLIENT_ID = "f78d31a8887d509";
+
+    let myHeaders = new Headers();
+    myHeaders.append("Authorization", `Client-ID ${CLIENT_ID}`);
+
+    let formdata = new FormData();
+    formdata.append("image", file);
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: formdata,
+      redirect: "follow",
+    };
+
+    return fetch("https://api.imgur.com/3/image", requestOptions)
+      .then((response) => response.text())
+      .then((result) => JSON.parse(result).data.link)
+      .catch((error) => console.log("error", error));
+  }
+
+  async function handlePhotoPicked(e) {
+    if (!e.target.files[0]) return;
+    if (e.target.files[0].size > 10485760) {
+      alert("File is too big. Max size is 10MB.");
+      return;
+    }
+
+    setIsLoading(true);
+    const imgUrlResponse = await uploadImage(e.target.files[0]);
+    setImgUrl(imgUrlResponse);
+    setIsLoading(false);
   }
 
   function handleRemovePhotoBtnClicked() {
-    photoPreviewRef.current.hidden = true;
-    photoPickerRef.current.hidden = false;
-    photoPickerInput.current.value = "";
+    setImgUrl("");
   }
 
   function handleNewPostBtnClicked() {
@@ -38,6 +67,11 @@ function NewPostBtn() {
     textInputRef.current.style.minHeight = "0px";
     textInputRef.current.style.minHeight =
       textInputRef.current.scrollHeight + "px";
+  }
+
+  function handleTextInputChanged(e) {
+    resizeTextInput();
+    setPostContent(e.target.value);
   }
 
   return (
@@ -81,7 +115,8 @@ function NewPostBtn() {
                 placeholder="What's on your mind, Clarence?"
                 maxLength={1500}
                 ref={textInputRef}
-                onChange={resizeTextInput}
+                onChange={handleTextInputChanged}
+                value={postContent}
               ></textarea>
               <input
                 type="file"
@@ -89,39 +124,42 @@ function NewPostBtn() {
                 id="photo-picker-input"
                 accept="image/png, image/jpeg"
                 onChange={handlePhotoPicked}
-                ref={photoPickerInput}
                 hidden
               />
-              <label
-                htmlFor="photo-picker-input"
-                className="photo-picker-label"
-                ref={photoPickerRef}
-              >
-                <div className="prompt">
-                  <Image weight="fill" className="icon" />
-                  Add a photo to your post
-                </div>
-              </label>
-              <div
-                className="photo-preview-container"
-                ref={photoPreviewRef}
-                hidden
-              >
-                <img
-                  src="https://i.imgur.com/w0EF8bN.jpeg"
-                  alt=""
-                  className="photo-preview"
-                />
-                <button
-                  type="button"
-                  className="remove-btn"
-                  onClick={handleRemovePhotoBtnClicked}
+              {!imgUrl && !isLoading ? (
+                <label
+                  htmlFor="photo-picker-input"
+                  className="photo-picker-label"
                 >
-                  ✕
-                </button>
-              </div>
+                  <div className="prompt">
+                    <Image weight="fill" className="icon" />
+                    Add a photo to your post
+                  </div>
+                </label>
+              ) : (
+                <div className="photo-preview-container">
+                  {isLoading ? (
+                    <Throbber />
+                  ) : (
+                    <div>
+                      <img src={imgUrl} alt="" className="photo-preview" />
+                      <button
+                        type="button"
+                        className="remove-btn"
+                        onClick={handleRemovePhotoBtnClicked}
+                      >
+                        ✕
+                      </button>
+                    </div>
+                  )}
+                </div>
+              )}
             </div>
-            <button type="button" className="post-btn">
+            <button
+              type="button"
+              className={"post-btn"}
+              disabled={!postContent}
+            >
               Post
             </button>
           </form>
---
+++ src/components/Throbber.js
@@ -0,0 +1,15 @@
+import "../styles/Throbber.css";
+
+function Throbber() {
+  return (
+    <div className="Throbber">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </div>
+  );
+}
+
+export default Throbber;
