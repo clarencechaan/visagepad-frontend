5cb27f299defe819f8bcb76b22091a772b2a9291
Clarence Chan <clarencechaan@icloud.com>
2023-09-15 12:18:19 -0400

    Remove mention of Render Web Service

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -5,10 +5,7 @@ function LoadingScreen({ isLoading }) {
   return (
     <div className={"LoadingScreen" + (isLoading ? "" : " invisible")}>
       <DotsThrobber />
-      <div className="message">
-        Please be patient as the Render Web Service wakes up. This may initially
-        take 15 to 30 seconds.
-      </div>
+      <div className="message">Loading</div>
     </div>
   );
 }
